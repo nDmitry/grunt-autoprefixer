912dcf904aac4e8730e3c45b2282d39e1c75170b
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2013-04-24 09:47:50 +0700

    Remove test for default options

--- test/autoprefixer_test.js
+++ test/autoprefixer_test.js
@@ -27,15 +27,6 @@ exports.autoprefixer = {
         // setup here if necessary
         done();
     },
-    default_options: function (test) {
-        test.expect(1);
-
-        var actual = grunt.file.read('tmp/default_options.css');
-        var expected = grunt.file.read('test/expected/default_options.css');
-        test.strictEqual(actual, expected, 'should describe what the default behavior is.');
-
-        test.done();
-    },
     custom_options: function (test) {
         test.expect(1);
 
